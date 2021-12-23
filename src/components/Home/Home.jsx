2bac45bf4019514ff6fc9d5a20f74a655bd5a0d1
IbrahimAbuawad <ibrahimabuawadwork@gmail.com>
2021-12-24 00:30:11 +0200

    Done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,21 +3,37 @@ import ArtWork from "../ArtWork/ArtWork";
 import Header from "../Header/Header";
 import MainText from "../MainText/MainText";
 import Services from "../Services/Services";
-import './home.css'
+import "./home.css";
 function Home() {
   return (
-    <div id="container">
-      <div id="leftside">
-        <Header />
-        <div id='main'>
-          <MainText />
-          <Services />
+    <>
+      <div id="container">
+        <div class="leftside">
+          <Header />
+          <div class="main">
+            <MainText />
+            <Services />
+          </div>
+        </div>
+        <div class="rightside">
+          <ArtWork />
         </div>
       </div>
-      <div id="rightside">
-        <ArtWork />
+
+      <div id="container2">
+       
+          <Header />
+        <div class="rightside">
+          <ArtWork />
+        </div>
+        <div class="leftside">
+          <div class="main">
+            <MainText />
+            <Services />
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -7,42 +7,48 @@ import "./services.css";
 
 function Services() {
   return (
-    <div id="servcontainer">
-      <div>
+    <div  id="servcontainer">
+      <div data-aos="flip-left"
+     data-aos-easing="ease-out-cubic"
+     data-aos-duration="2000">
         <img src={firstSvg} alt="first" />
         <h4>Web Design</h4>
         <p>
           Sed ut perspiciatis unde omni iste natus error sit volunteer
-          accusantum doloremque.{" "}
+          accusantum doloremque.
         </p>
         <IoIosArrowDropright
           className="circlearrow"
-          size="30px"
+          size="55px"
           color="#DF0D25"
         />
       </div>
-      <div>
+      <div data-aos="flip-left"
+     data-aos-easing="ease-out-cubic"
+     data-aos-duration="1500">
         <img src={secondSvg} alt="second" />
         <h4>Digital Artwork</h4>
         <p>Sed ut perspiciatis unde omni iste natus </p>
         <IoIosArrowDropright
           className="circlearrow"
-          size="30px"
+          size="55px"
           color="#0071D8"
         />
       </div>
-      <div>
+      <div data-aos="flip-left"
+     data-aos-easing="ease-out-cubic"
+     data-aos-duration="2500">
         <img src={thirdSvg} alt="third" />
         <h4>Website Development</h4>
         <p>
           Sed ut perspiciatis unde omni iste natus error sit volunteer
-          accusantum doloremque. Ut perspiciatis unde omni iste natus error sit
-          volunteer accusantum doloremque.
+          accusantum doloremque. Ut perspiciatis unde omni 
         </p>
         <IoIosArrowDropright
           className="circlearrow"
-          size="30px"
+          size="55px"
           color="#00D879"
+          
         />
       </div>
     </div>
